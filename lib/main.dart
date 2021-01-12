aeec20cbcbccd07cf190af21267ce8c95b2b4ab6
fkn1017 <kosuke.fukano@gmail.com>
2021-01-12 19:41:27 +0900

    カスタムしたCupertinoContextMenuを追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:custom_cupertino_context_menu/custom_cupertino_context_menu.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -5,113 +7,102 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class MyHomePage extends StatelessWidget {
+  final List<Widget> _actions = [
+    CupertinoContextMenuAction(child: Text('AAA')),
+    CupertinoContextMenuAction(child: Text('BBB')),
+    CupertinoContextMenuAction(child: Text('CCC')),
+    CupertinoContextMenuAction(child: Text('DDD')),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    final Widget _officialCuperTinoContextMenu = Column(
+      children: [
+        Text(
+          'CuperTinoContextMenu',
+          style: TextStyle(fontSize: 20),
+        ),
+        CupertinoContextMenu(
+          actions: _actions,
+          child: TappablePhoto(
+            onTap: () => _moveToNewPage(context),
+          ),
+        ),
+      ],
+    );
+    final Widget _customCupertinoContextMenu = Column(
+      children: [
+        Text(
+          'Custom CuperTinoContextMenu',
+          style: TextStyle(fontSize: 20),
+        ),
+        CustomCupertinoContextMenu(
+          actions: _actions,
+          child: TappablePhoto(
+            onTap: () => _moveToNewPage(context),
+          ),
+        ),
+      ],
+    );
+
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
+      appBar: AppBar(),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            _officialCuperTinoContextMenu,
+            _customCupertinoContextMenu,
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  void _moveToNewPage(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (BuildContext context) {
+        return Scaffold(
+          appBar: AppBar(),
+          body: Center(
+            child: ColorFiltered(
+              colorFilter: ColorFilter.mode(Colors.green, BlendMode.colorBurn),
+              child: Image.asset('lib/lena.png'),
+            ),
+          ),
+        );
+      }),
+    );
+  }
+}
+
+class TappablePhoto extends StatelessWidget {
+  TappablePhoto({this.onTap});
+
+  VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: GestureDetector(
+        onTap: onTap,
+        child: Image.asset('lib/lena.png', width: 200),
+      ),
     );
   }
 }
